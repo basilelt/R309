53ead1535367d3486c43bc8c2512c60195f041c9
Basile <lethiecb@protonmail.com>
2023-11-08 17:37:02 +0100

    Add chat functionality with client and server modules

--- TP2/Ex1/client/client.py
+++ TP2/Ex1/client/client.py
@@ -1,8 +1,96 @@
-import socket
+"""
+Ce module contient la logique du client pour une application de chat.
 
-def client(address, port):
-    client_socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket_tcp.connect((address, port))
-    client_socket_tcp.send("Hello, World!".encode())
-    data = client_socket_tcp.recv(1024).decode()
-    client_socket_tcp.close()
+Il définit trois fonctions principales : `client`, `send` et `listen`.
+
+Fonctions
+---------
+client(user:str, host: str, port: int) -> None
+    Établit une connexion avec le serveur à l'adresse et au port spécifiés, et lance deux threads pour envoyer et recevoir des messages.
+
+send(socket: socket.socket, user:str) -> None
+    Envoie des messages à travers la socket spécifiée. Les messages sont lus à partir de l'entrée standard.
+
+listen(socket: socket.socket) -> None
+    Écoute les messages sur la socket spécifiée et les affiche sur la sortie standard.
+
+
+Lève
+------
+socket.gaierror
+    Si une erreur se produit lors de la résolution de l'hôte.
+ConnectionRefusedError
+    Si la connexion est refusée.
+TimeoutError
+    Si une opération de socket dépasse le délai d'attente.
+KeyboardInterrupt
+    Si l'utilisateur interrompt le programme.
+ConnectionResetError
+    Si la connexion est réinitialisée pendant l'envoi d'un message.
+"""
+
+import socket, threading, queue
+from pynput import keyboard
+
+input_queue = queue.Queue()
+
+def client(user:str, host:str, port:int) -> None:
+    flag = False
+    while not flag:
+        try:
+            client_socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket_tcp.connect((host, port))
+            threading.Thread(target=send, args=(client_socket_tcp, user)).start()
+            threading.Thread(target=listen, args=(client_socket_tcp,)).start()
+        except(socket.gaierror):
+            print("Erreur lors de la résolution de l'hôte")
+        except(ConnectionRefusedError):
+            print("Connexion refusée")
+        except(TimeoutError):
+            print("Timeout")
+        except KeyboardInterrupt as err:
+            print("Client en cours d'extinction...")
+            if err.args:
+                if err.args[0] == "arret":
+                    client_socket_tcp.send("arret".encode())
+            else:
+                client_socket_tcp.send("bye".encode())
+            client_socket_tcp.close()
+            break
+        except Exception as err:
+            print(err)
+
+def on_press(key):
+    try:
+        input_queue.put(key.char) # Essaie de récupérer le caractère associé à la touche
+    except AttributeError:
+        pass # Touche spéciale (flèches, ctrl, alt, etc.)
+
+
+def send(socket:socket.socket, user) -> None:
+    while True:
+        try:
+            data = ''
+            while not input_queue.empty():  # Tant que la file n'est pas vide
+                data += input_queue.get()  # Prend le prochain caractère
+            if data == "arret":
+                raise KeyboardInterrupt(data)
+            elif data == "bye":
+                raise KeyboardInterrupt(data)
+            elif data:
+                socket.send((user+data).encode())
+        except(ConnectionResetError):
+            print("Connexion réinitialisée")
+        except(BrokenPipeError):
+            print("Rupture de la connexion")
+
+def listen(socket:socket.socket) -> None:
+    while True:
+        try:
+            data = socket.recv(1024).decode()
+            if data:
+                print(data)
+        except(ConnectionResetError):
+            print("Connexion réinitialisée")
+        except(BrokenPipeError):
+            print("Rupture de la connexion")
--- TP2/Ex1/server/server.py
+++ TP2/Ex1/server/server.py
@@ -1,13 +1,78 @@
-import socket
+"""
+Ce module contient la logique du serveur pour une application de chat.
 
-def server(port):
-    try:
-        server_socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-    server_socket_tcp.bind(("0.0.0.0", port))
+Il définit deux fonctions principales : `server` et `handler`.
+
+Fonctions
+---------
+server(host:str, port: int) -> None
+    Démarre le serveur sur le port spécifié, accepte les connexions entrantes et lance un thread pour chaque client pour gérer la communication.
+
+handler(connections: dict, conn: socket.socket) -> None
+    Gère la communication avec un client spécifique. Si le client envoie "bye", la connexion est fermée. Si le client envoie "arret", une exception KeyboardInterrupt est levée.
+
+Lève
+------
+socket.gaierror
+    Si une erreur se produit lors de la résolution de l'hôte.
+ConnectionRefusedError
+    Si la connexion est refusée.
+TimeoutError
+    Si une opération de socket dépasse le délai d'attente.
+KeyboardInterrupt
+    Si l'utilisateur interrompt le programme.
+ConnectionResetError
+    Si la connexion est réinitialisée pendant l'envoi d'un message.
+BrokenPipeError
+    Si une erreur de pipe cassé se produit lors de l'envoi d'un message.
+Exception
+    Si une autre exception est levée.
+"""
+
+import socket
+import threading
+
+def server(host:str, port:int) -> None:
+    connections = {}
+    server_socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket_tcp.bind((host, port))
     server_socket_tcp.listen(1)
-    conn, address = server_socket_tcp.accept()
-    data = conn.recv(1024).decode()
-    conn.send(data.encode())
-    conn.close()
-    
+    while True:
+        try:
+            conn, address = server_socket_tcp.accept()
+            connections[conn] = address
+            client_thread = threading.Thread(target=handler, args=(connections,conn))
+            client_thread.start()
+        except(socket.gaierror):
+            print("Erreur lors de la résolution de l'hôte")
+        except(ConnectionRefusedError):
+            print("Connexion refusée")
+        except(TimeoutError):
+            print("Timeout")
+        except KeyboardInterrupt:
+            print("Serveur en cours d'extinction...")
+            for conn in connections:
+                conn.close()
+            server_socket_tcp.close()
+            break
+        except Exception as err:
+            print(err)
+
+def handler(connections:dict, conn:socket.socket) -> None:
+    try:
+        data = conn.recv(1024).decode()
+        if data == "bye":
+            del connections[conn]
+            conn.close()
+        elif data == "arret":
+            raise KeyboardInterrupt
+        else:
+            for client in connections:
+                if client != conn:
+                    client.send(data.encode())
+    except(ConnectionResetError):
+        print("Connexion réinitialisée")
+    except(BrokenPipeError):
+        print("Rupture de la connexion")
+
+        
